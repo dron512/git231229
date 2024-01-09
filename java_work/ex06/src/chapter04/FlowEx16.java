93dde7bbf5bbee234cbd4fc56365c0c070623e47
dron512 <parkmyounghoi@gmail.com>
2024-01-09 16:25:48 +0900

    240109

--- java_work/ex06/src/chapter04/FlowEx16.java
+++ java_work/ex06/src/chapter04/FlowEx16.java
@@ -1,9 +1,18 @@
 package chapter04;
+
 class FlowEx16 {
-	public static void main(String[] args) { 
-		for(int i=1;i<=5;i++) { 
-			for(int j=1;j<=10;j++) {
-				System.out.print("*");			
+	public static void main(String[] args) {
+
+		// i=1 일떄 j = 1~10
+		// i=2 일때 j = 1~10
+		// i=3 일때 j = 1~10
+		// i=4 일때 j = 1~10
+		// i=5 일떄 j = 1~10
+		for (int i = 1; i <= 5; i++) {
+//			System.out.println("i = " + i);
+			for (int j = 1; j <= 10; j++) {
+//				System.out.println(" j = " + j);
+				System.out.print("*");
 			}
 			System.out.println();
 		}
--- java_work/ex06/src/chapter04/FlowEx17.java
+++ java_work/ex06/src/chapter04/FlowEx17.java
@@ -1,21 +1,21 @@
 package chapter04;
+
 import java.util.*;
 
 class FlowEx17 {
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		int num = 0;
 
-		System.out.print("*�� ����� ������ ���� �Է��ϼ���.>");
-
 		Scanner scanner = new Scanner(System.in);
-		String tmp = scanner.nextLine();  // ȭ���� ���� �Է¹��� ������ tmp�� ����
-		num = Integer.parseInt(tmp);      // �Է¹��� ���ڿ�(tmp)�� ���ڷ� ��ȯ
+		String tmp = scanner.nextLine();
+		num = Integer.parseInt(tmp);
 
-		for(int i=0;i<num;i++) {
-			for(int j=0;j<=i;j++) {
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-	} // main�� ��
+
+	}
 }
