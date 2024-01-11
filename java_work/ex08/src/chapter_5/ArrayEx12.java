fb12f4537efcd3b693010ad271918280902a1d51
dron512 <parkmyounghoi@gmail.com>
2024-01-11 16:10:47 +0900

    240111

--- java_work/ex08/src/chapter_5/ArrayEx12.java
+++ java_work/ex08/src/chapter_5/ArrayEx12.java
@@ -1,18 +1,22 @@
 package chapter_5;
-class ArrayEx12 {
-	public static void main(String[] args) {
-		String[] names = {"Kim", "Park", "Yi"};
 
-		for(int i=0; i < names.length;i++) {
-			System.out.println("names["+i+"]:"+names[i]);
+class ArrayEx12 {
+	
+	public static void main(String[] args) {
+		
+		String[] names = { "Kim", "Park", "Yi" };
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("names[" + i + "]:" + names[i]);
 		}
 
-		String tmp = names[2]; // �迭 names�� �� ��°��Ҹ� tmp�� ����
-		System.out.println("tmp:"+tmp);
+		String tmp = names[2];
+		System.out.println("tmp:" + tmp);
 
-		names[0] = "Yu"; // �迭 names�� ù ��° ��Ҹ� "Yu"�� ����
+		names[0] = "Yu";
 
-		for(String str : names)   // ���� for��
+		for (String str : names)
 			System.out.println(str);
 	} // main
+	
 }
