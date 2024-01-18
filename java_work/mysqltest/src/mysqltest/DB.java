abc8bd99e667d953624ef6bd6afe5fb756a80866
dron512 <parkmyounghoi@gmail.com>
2024-01-18 17:08:20 +0900

    240118

--- java_work/mysqltest/src/mysqltest/DB.java
+++ java_work/mysqltest/src/mysqltest/DB.java
@@ -3,9 +3,17 @@ package mysqltest;
 public class DB {
 
 	public void insert() {
+		System.out.println("DB에 할일입력넣기");
+		
+		try {
+			
+		}catch (Exception e) {
+		}
 		
 	}
+	
 	public void select() {
-		
+		System.out.println("DB에 할일목록 보여주기");
 	}
+	
 }
