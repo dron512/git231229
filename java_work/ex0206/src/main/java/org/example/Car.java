efc5a8c416c58a6405ea706834e262f46c3ee7c0
dron512 <parkmyounghoi@gmail.com>
2024-02-06 17:17:51 +0900

    240206

---
+++ java_work/ex0206/src/main/java/org/example/Car.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class Car {
+
+    private String name;
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    //    public String getName() {
+//        return name;
+//    }
+
+    //    public Car(String name){
+//        this.name = name;
+//    }
+
+
+//    @Override
+//    public String toString() {
+//        return "Car{" +
+//                "name='" + name + '\'' +
+//                '}';
+//    }
+}
