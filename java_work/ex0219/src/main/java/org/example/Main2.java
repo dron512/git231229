11200b15523ae7b370f4641cb55ab022bbf98410
dron512 <parkmyounghoi@gmail.com>
2024-02-19 11:25:48 +0900

    0219

---
+++ java_work/ex0219/src/main/java/org/example/Main2.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Main2 {
+    public static void main(String[] args) {
+        try (
+            FileInputStream fis = new FileInputStream("aaa.pptx");
+            FileOutputStream fos = new FileOutputStream("ccc.pptx");
+        ) {
+            byte[] buf = new byte[1024];
+            int len;
+            while (true) {
+                len = fis.read(buf);
+                if (len == -1) {
+                    break;
+                }
+                fos.write(buf, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
