ff84e05fb6cde8d221d370d2694e7cbe7179dbee
dron512 <parkmyounghoi@gmail.com>
2024-02-28 11:59:00 +0900

    240228

---
+++ spring_work/ex04/READMD.md
@@ -0,0 +1,7 @@
+mybatis 홈페이지 
+
+https://mybatis.org/mybatis-3/ko/configuration.html
+
+<!DOCTYPE mapper
+    PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN"
+    "https://mybatis.org/dtd/mybatis-3-mapper.dtd">
---
+++ spring_work/ex04/src/main/java/org/example/components/MemberService.java
@@ -0,0 +1,30 @@
+package org.example.components;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService implements InitializingBean, DisposableBean {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private String str;
+
+    public void regist(){
+        memberRepository.insert();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        str = "jdbc:mysql://192.168.";
+        System.out.println("IOC컨테이너 들어갈때 ...");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("IOC 컨테이너 없어질떄...");
+    }
+}
