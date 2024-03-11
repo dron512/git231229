504771878e1e860a2cee6c885fb9dc60485e7751
dron512 <parkmyounghoi@gmail.com>
2024-03-11 09:32:51 +0900

    240311

--- springbootApi_work/restapi03/src/main/java/com/mh/restapi03/conf/CorsConfig.java
+++ springbootApi_work/restapi03/src/main/java/com/mh/restapi03/conf/CorsConfig.java
@@ -6,13 +6,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET","POST","PUT","DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowedMethods("*");
+
     }
+
+    //    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET","POST","PUT","DELETE")
+//                .allowedHeaders("*");
+//    }
 
 }
