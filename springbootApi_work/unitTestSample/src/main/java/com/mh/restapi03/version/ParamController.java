8eef1b17d4a9c89362e73b18085eeef004b7eea0
dron512 <parkmyounghoi@gmail.com>
2024-03-13 17:32:13 +0900

    240313

--- springbootApi_work/unitTestSample/src/main/java/com/mh/restapi03/version/ParamController.java
+++ springbootApi_work/unitTestSample/src/main/java/com/mh/restapi03/version/ParamController.java
@@ -6,16 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-public class HeaderController {
+public class ParamController {
 
-    @GetMapping(value = "header",headers = "version=1")
+    @GetMapping(value = "param",params = "v1=1")
     public String paramV1(){
-        return "headersV1";
+        return "paramV1";
     }
-
-    @GetMapping(value = "header",headers = "version=2")
+    @GetMapping(value = "param",params = "v1=2")
     public String paramV2(){
-        return "headersV2";
+        return "paramV2";
     }
-
 }
