f219c44b1b6b309924a1a2db9118ad7d77666b4a
dron512 <parkmyounghoi@gmail.com>
2024-03-26 12:57:05 +0900

    240326

--- html_work/3project/login/js/login.js
+++ html_work/3project/login/js/login.js
@@ -1,21 +1,14 @@
 function doLogin() {
-    console.log('일로오나');
-    // 로그인할 이메일과 비밀번호를 변수에 담는다
     const loginemail = document.getElementById("loginemail");
     const loginpassword = document.getElementById("loginpassword");
     // fetch("http://112.218.211.194:8888/user/signIn", {
-        fetch("http://localhost:8080/user/signIn", {
-            // method는 get, post, put, delete가 있다 
-            // 백단과 맞게 적기 (노션참고)
+        fetch("http://112.218.211.194:10049/user/signIn", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            // post매핑을 쓰기 때문에 바디로 보낸다
-            // 이때 json형식으로 보내겠다는 뜻
             body: JSON.stringify(
                 {
-                    // 보낼 데이터가 들어갈 컬럼(컬럼명이 틀리면 안됨) : 보낼 데이터
                     email: loginemail.value,
                     password: loginpassword.value,
                 }
@@ -36,6 +29,7 @@ function doLogin() {
             location.href = 'main.html';
         })
         .catch(error => {
+            alert('로그인실패');
             console.error(error);
         });
 
