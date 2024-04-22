e604c1e340fa598d3ca8402f756975485c8c7be8
dron512 <parkmyounghoi@gmail.com>
2024-04-22 12:41:11 +0900

    240422

--- front_work/ex04/src/components/Person.js
+++ front_work/ex04/src/components/Person.js
@@ -12,10 +12,20 @@ const getPeson = async () => {
     return result.data;
 }
 
+const getData = async () => {
+    console.log('getData');
+    const id = parseInt(Math.random() * 3 + 1);
+
+    const result = await axios.get(`http://www.grencoffee.store/user/`);
+    console.log(result);
+    return result.data;
+}
+
 const Person = ({ name: personname, age: paramage }) => {
     console.log('Person');
     const [age, setAge] = useState(paramage);
     const [name, setName] = useState(personname);
+    const [data,setData] = useState("초기값");
     const addAge = () => {
         setAge(age + 1);
     }
@@ -23,6 +33,7 @@ const Person = ({ name: personname, age: paramage }) => {
         setAge(age - 1);
     }
     return (<>
+        <h1>{data}</h1>
         <h1>Person</h1>
         <h2>이름 = {name}</h2>
         <h2>나이 = {age}</h2>
@@ -33,6 +44,10 @@ const Person = ({ name: personname, age: paramage }) => {
             const data = await getPeson();
             setAge(data.age);
             setName(data.name);
+        }}>가져오기</button>
+        <button onClick={async () => {
+            const data = await getData();
+            setData(data);
         }}>가져오기</button>
     </>);
 }
---
+++ front_work/ex04/src/components/Person1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Person1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Person1</h2>
+      </div>
+    );
+  }
+}
