b94e7218b1488c7573fe08837e05804a5aef67f2
dron512 <parkmyounghoi@gmail.com>
2024-04-30 20:17:36 +0900

    mac

--- react_work/nanatest/src/pages/aa/AA.js
+++ react_work/nanatest/src/pages/aa/AA.js
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
 import { aa } from '../../api/aa/aa';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export const AA = () => {
 
     const [data, setData] = useState("asdnfklcjqwnelrkjewnr");
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-        <div>
+        <div className='container'>
             <h1>구분을</h1>
             <h2>{data}</h2>
-            <button onClick={() => {
+            <button className='btn btn-primary p-3' onClick={() => {
                 aa(setData);
             }}>버튼</button>
 
+
+            <div
+                className="modal show"
+                style={{ display: 'block', position: 'initial' }}
+            >
+                <Button variant="primary" onClick={handleShow}>
+                    Launch demo modal
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         </div>
     )
 }
