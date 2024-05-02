90c6186e3754d475d07c4ea98b60d88e39048cbb
dron512 <parkmyounghoi@gmail.com>
2024-05-02 09:28:47 +0900

    main

--- springbootApi_work/mychart/src/main/java/com/mh/mychart/MychartApplication.java
+++ springbootApi_work/mychart/src/main/java/com/mh/mychart/MychartApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.swing.text.html.parser.Entity;
-
 @SpringBootApplication
 @EnableFeignClients // FeignClient 사용을 위한 활성화
 public class MychartApplication {
